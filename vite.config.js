6560dfd5488822db44cdb84d1551501b2263ca2d
mnadalin <massimo.nadalin@devangarde.it>
2025-02-28 18:44:33 +0100

    index footer

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite'
+import { createHtmlPlugin } from 'vite-plugin-html';
+import { readFileSync } from 'fs';
 import vue from '@vitejs/plugin-vue'
 import packageJson from './package.json'
 
+const pkg = JSON.parse(readFileSync('./package.json', 'utf-8'));
+
 export default defineConfig({
 	base: '/vue3-bootstrap-typeahead/',
-	plugins: [vue()],
+	plugins: [
+		vue(),
+		createHtmlPlugin({
+			inject: {
+				data: {
+					version: pkg.version,
+				},
+			},
+		}),
+	],
 	define: {
 		'import.meta.env.PACKAGE_VERSION': JSON.stringify(packageJson.version)
 	},
